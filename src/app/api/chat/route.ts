d24f47711d6175aa7651a72100060a55ce16bef6
José Núñez <79015124+josiext@users.noreply.github.com>
2023-11-02 18:50:35 -0300

    updates

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,31 @@
+import { CohereTextGenerationModel, generateText } from "modelfusion";
+
+export async function POST(req: Request) {
+  const { messages } = (await req.json()) as {
+    messages: {
+      label: string;
+      message: string;
+    }[];
+  };
+
+  console.log(messages);
+
+  const initialPromp = `Eres un asistente virtual que habla en español. Ayudas a la gente a resolver sus dudas. Vas a recibir un historial con las dudas que han consultado y las que has respondido. Vas a responder a la ultima pregunta del usuario. Se conciso. Habla solo en español. Solo responde el ultimo mensaje del usuario y nada más. \n\n`;
+
+  const promps = messages.reduce((acc, message) => {
+    return `${acc} ${message.label}: ${message.message} \n\n`;
+  }, initialPromp);
+
+  console.log(promps);
+
+  const text = await generateText(
+    new CohereTextGenerationModel({
+      model: "command-nightly",
+      temperature: 0.7,
+      maxCompletionTokens: 500,
+    }),
+    promps
+  );
+
+  return Response.json({ message: text });
+}
