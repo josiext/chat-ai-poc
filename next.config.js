5cef660e38e5db541a87c4cb982bb6255b2e7c56
José Núñez <79015124+josiext@users.noreply.github.com>
2023-11-16 17:14:49 -0300

    remove local model configs

--- next.config.js
+++ next.config.js
@@ -3,18 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse"],
   },
-
-  output: "export", // Feel free to modify/remove this option
-
-  webpack: (config) => {
-    // See https://webpack.js.org/configuration/resolve/#resolvealias
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      sharp$: false,
-      "onnxruntime-node$": false,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
