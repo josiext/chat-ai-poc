eb848af677ee0f4e9d845aa31e37bbf1ffd29437
José Núñez <79015124+josiext@users.noreply.github.com>
2023-11-17 12:17:37 -0300

    ui

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,8 +1,13 @@
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { fetcher } from "@/utils/fetcher";
 import Link from "next/link";
+import useSWR from "swr";
+
+const API_URL = "https://jsonplaceholder.typicode.com/todos/1";
 
 export default function Documents() {
+  const { data = [], error, isLoading } = useSWR(API_URL, fetcher);
+
   return (
     <div className="flex flex-col flex-grow p-4 gap-4">
       <h1 className="text-2xl font-semibold text-neutral-900">Documentos</h1>
